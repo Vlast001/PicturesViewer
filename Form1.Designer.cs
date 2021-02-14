9263a1471dd6724ee6a70cd25c10c22c0803be41
Stark <anfernoangel@gmail.com>
2021-02-14 12:22:02 +0200

    add icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,15 @@ namespace PicturesViewer
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(715, 327);
+            this.ClientSize = new System.Drawing.Size(930, 473);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Simple Picture Viewer";
             this.ResumeLayout(false);
